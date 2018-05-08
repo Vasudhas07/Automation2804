0d105da62dde5172cb007df6d36498ab7984977f
PC <PC@PC-PC>
2018-05-08 10:15:29 +0530

    2804E2E

---
+++ yuio/src/yuio/A.java
@@ -0,0 +1,382 @@
+package yuio;
+
+import java.util.Scanner;
+
+public class A {
+
+	public static void main(String[] args) {
+		/*for(int i=7;i>=1;i--)
+			{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print(j);
+			}
+			System.out.println();
+			}
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		
+		
+		for(int i=1;i<=7;i++)
+		{
+		for(int j=7;j>=i;j--)
+		{
+			System.out.print(j);
+		}
+		System.out.println();
+		}
+		
+		System.out.println();
+		System.out.println();
+		System.out.println(); 
+		
+		for(int i=7;i>=1;i--)
+		{
+			for(int j=7;j>=i;j--)
+			{
+				System.out.print(j);
+			
+			System.out.println();
+		}*/
+		
+		
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		
+		/*for(int i=6;i>=1; i--)
+		{
+			for(j=1;j<=i;j++)
+			{
+				System.out.print("1");
+				//System.out.print(j);
+			}
+			for(int k=i+1;k>=7;k--)
+			{
+			for(int l=1;l<=7;l++)
+			{
+				
+			System.out.print(l);
+			}
+			//System.out.println();
+			}
+		System.out.println();
+		}*/
+		
+		
+		
+		
+		
+		int n;
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("How many rows do you want to print");
+		int rows=sc.nextInt();
+		
+		/*for(int m=1;m<=rows;m++)
+		{
+			for(n=1;n<=(rows-m);n++)
+			{	
+				System.out.print("1");
+			}
+				
+					for(int o=1;o<=m;o++)
+					{
+						System.out.print(m);
+
+					}
+				
+				
+			
+			System.out.println();
+		}*/
+		
+//		for(int i=1;i<=rows;i++)
+//		{
+//			for(int j1=1;j1<=i;j1++)
+//			{
+//				System.out.print(j1);
+//			
+//			
+//			if(j1==rows)
+//			{
+//				System.out.println();
+//				for(int k=rows-1;k>=1;k--)
+//				{
+//					for(int l=1;l<=k;l++)
+//						{
+//						
+//						System.out.print(l);
+//						}
+//					System.out.println();
+//				}
+//			}
+//			}
+//			System.out.println();
+//		}
+//		
+//		
+//		
+//		System.out.println();
+//		System.out.println();
+//		System.out.println();
+//		System.out.println();
+//		
+//		int j2;
+//		for(int i=rows;i>=1;i--)
+//		{
+//			for(j2=1;j2<=i;j2++)
+//			{
+//				System.out.print(j2);
+//			}
+//			System.out.println();
+//			
+//			if(i==1)
+//			{
+//				for(int k=1;k<=rows;k++)
+//				{
+//					for(int l=1;l<=k;l++)
+//					{
+//						System.out.print(l);
+//					}
+//					System.out.println();
+//				}
+//			}
+//		}
+//		
+//		
+//		System.out.println();
+//		System.out.println();
+//		System.out.println();
+//		System.out.println();
+//		
+//		
+//		for(int i=1;i<=rows;i++)
+//		{
+//			for(int j=1;j<=i;j++)
+//			{
+//				System.out.print(j);
+//			}
+//			
+//			
+//			for(int k=i-1;k>=1;k--)
+//			{
+//				System.out.print(k);
+//			}
+//			System.out.println();
+//			
+//		}
+//		
+//		
+//		System.out.println();
+//		System.out.println();
+//		System.out.println();
+//		System.out.println();
+		
+		
+		/*int i;
+		for(i=1;i<=rows;i++)
+		{
+			for(int j=1;j<i;j++)
+			{
+				System.out.print(" ");
+			}
+			for(int k=i;k<=rows;k++)
+			{
+				System.out.print(k);
+			}
+			System.out.println();
+		}
+		for(int l=rows-1;l>=1;l--)
+		{
+			for(int m=1;m<l;m++)
+			{
+				System.out.print(" ");
+			}
+			for(int n1=l;n1<=rows;n1++)
+			{
+				System.out.print(n1);
+			}
+			System.out.println();
+		}
+		
+		
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();*/
+		
+		
+		/*for(int i1=1;i1<=rows;i1++)
+		{
+			for( int j=1;j<=i1;j++)
+				{
+					if(j%2==0)
+					{
+						
+						System.out.print("0");
+					}
+					else
+					{
+						System.out.print("1");
+					}
+				}
+			System.out.println();
+		}*/
+		
+		
+		
+		int i;
+		/*for(i=1;i<=rows;i++)
+		{
+			for(int j=1;j<i;j++)
+			{
+				System.out.print(" ");
+			}
+			for(int k=i;k<=rows;k++)
+			{
+				System.out.print(k + " ");
+			}
+			System.out.println();
+		}*/
+		for(int l=rows-1;l>=1;l--)
+		{
+			for(int m=1;m<l;m++)
+			{
+				System.out.print(" ");
+			}
+			for(int n1=l;n1<=rows;n1++)
+			{
+				System.out.print(n1 + " ");
+			}
+			System.out.println();
+		}
+		
+		
+		
+//		for (int i = 1; i <= rows; i++) 
+//		{
+//			int num = i;
+//			
+//			for (int j = 1; j <= i; j++) 
+//			{
+//				System.out.print(num+" ");
+//				
+//				num = num+rows-j;
+//			}
+//			
+//			System.out.println();
+//		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		/*for(int i=1;i<=7;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+			System.out.print(j);
+			}
+			System.out.println();
+			if(i==7)
+			{
+				for(int k=6;k>=1;k--)
+				{
+					for(int l=1;l<=k;l++)
+					{
+						System.out.print(l);
+					}
+					System.out.println();
+				}
+			}
+		}*/
+		
+		
+		/*for(int i=7;i>=1;i--)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print(j);
+			}
+			System.out.println();
+			if(i==1)
+			{
+				for(int k=2;k<=7;k++)
+				{
+					for(int l=1;l<=k;l++)
+					{
+						System.out.print(l);
+					}
+					System.out.println();
+				}
+			}
+		}*/
+		
+		
+		/*for(int i=1;i<=7;i++)
+		{
+			for(int j=1;j<=i; j++)
+			{
+				System.out.print(j);
+			}
+			for(int k=i-1;k>=1;k--)
+			{
+				System.out.print(k);
+			}
+			System.out.println();
+		}*/
+		
+		
+		/*for(int i=1;i<=7;i++)
+		{
+			for(int j=i;j>=1;j--)
+			{
+				System.out.print(j);
+			}
+			System.out.println();
+		}*/
+		
+		
+		/*Scanner sc=new Scanner(System.in);
+		System.out.println("How many rows do you want");
+		int rows=sc.nextInt();
+		/*for(int i=0;i<rows;i++)
+		{
+			for(int j=0;j<rows;j++)
+			{
+				if(i==j)
+				{
+				System.out.print(i);
+				}
+				else
+				{
+					System.out.print("0");
+				}
+			}
+			System.out.println();
+		}*/
+		
+		
+		/*for(int i=0;i<rows;i++)
+		{
+			for(int j=0;j<rows;j++)
+			{
+				if((i==j) || (i+j)%2==0)
+				{
+					System.out.print(1);
+				}
+				else
+				{
+					System.out.print(0);
+				}
+			}
+			System.out.println();
+		}*/
+	}
+}
